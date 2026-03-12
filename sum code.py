2961779375587833e6dec1d637f082565c5a7a43
usman <u28259011@gmail.com>
2026-03-12 05:39:18 -0700

    Add files via upload
    first python program

---
+++ sum code.py
@@ -0,0 +1,10 @@
+var1 = float (input("Enter a value:\n")) # we can use float or int to convert the input into a number
+var2 = float (input(" Enter a value:\n")) # we can use float or int to convert the input into a number
+sum = var1+var2 # we can use any name for the variable instead of sum but it is not recommended to addition
+sub = var1-var2 # we can use any name for the variable instead of sum but it is not recommended to subtraction
+mult= var1*var2 # we can use any name for the variable instead of sum but it is not recommended to multiplication
+divi= var1/var2 # we can use any name for the variable instead of sum but it is not recommended to division
+print(sum) # printing the sum
+print(sub) # printing the difference
+print(mult) # printing the product
+print(divi) # printing the quotient
